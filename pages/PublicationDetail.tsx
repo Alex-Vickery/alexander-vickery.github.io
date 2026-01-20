773e6e1ed68c93aeb5401733cba6abf999d3346f
Alexander Vickery <alexvickery2018@gmail.com>
2026-01-20 16:56:49 +0000

    tidy up

--- pages/PublicationDetail.tsx
+++ pages/PublicationDetail.tsx
@@ -131,7 +131,7 @@ const PublicationDetail: React.FC = () => {
   return (
     <div className="relative">
       <motion.div
-        className="fixed top-16 left-0 right-0 h-1 bg-blue-600 origin-left z-50"
+        className="fixed top-16 left-0 right-0 h-1 bg-white origin-left z-50"
         style={{ scaleX }}
       />
 
